e56d9d4104d6e0426d9ee805d436361c3c520151
kerivin <root@LAPTOP-50JRBB77.>
2025-05-19 17:58:35 +0300

    build experiments

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -10,13 +10,13 @@ echo Installing LibreOffice SDK...
 choco install libreoffice-sdk -y --no-progress --params="/AddToPath"
 setx LibreOffice_DIR "C:\Program Files\LibreOffice\sdk" /M
 
-refreshenv
+call RefreshEnv.bat
 
 echo Installing SQLite3...
 choco install sqlite --installargs="ADDLOCAL=IncludeFiles,LibraryFiles" -y --no-progress
 setx SQLite3_DIR "C:\Program Files\SQLite" /M
 
-refreshenv
+call RefreshEnv.bat
 
 echo Installing Qt6 via choco...
 choco install qtcreator -y --no-progress
@@ -24,12 +24,12 @@ choco install qt6 -y --no-progress --params="ADD_TO_PATH=1"
 setx QT_DIR "C:\Qt\6.9.0\msvc2022_64" /M
 setx PATH "%%PATH%%;C:\Qt\6.9.0\msvc2022_64\bin" /M
 
-refreshenv
+call RefreshEnv.bat
 
 setx QT_DIR "C:\Qt\6.9.0\msvc2022_64" /M
 setx PATH "%%PATH%%;C:\Qt\6.9.0\msvc2022_64\bin" /M
 
-refreshenv
+call RefreshEnv.bat
 
 echo Verifying installations...
 where libreoffice
@@ -41,11 +41,11 @@ set PATH=%%PATH%%;%%ProgramFiles%%\LibreOffice\program
 set PATH=%%PATH%%;%%ProgramFiles%%\SQLite
 set PATH=%%PATH%%;%%QT_DIR%%\bin
 
-refreshenv
+call RefreshEnv.bat
 
 echo Required environment variables:
 echo - Qt6_DIR: %%QT_DIR%%
 echo - LibreOffice_DIR: %%LibreOffice_DIR%%
 echo - SQLite3_DIR: %%SQLite3_DIR%%
 
-refreshenv
+call RefreshEnv.bat
