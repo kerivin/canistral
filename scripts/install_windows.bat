5b824a12d506dafb6641a28d0a0c3766659eb1cf
kerivin <root@LAPTOP-50JRBB77.>
2025-05-19 17:55:00 +0300

    build experiments

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -1,9 +1,9 @@
 @echo off
 
-if not exist "%ProgramData%\chocolatey\bin\choco.exe" (
+if not exist "%%ProgramData%%\chocolatey\bin\choco.exe" (
     echo Installing Chocolatey...
     powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-    set PATH=%PATH%;%ProgramData%\chocolatey\bin
+    set PATH=%%PATH%%;%%ProgramData%%\chocolatey\bin
 )
 
 echo Installing LibreOffice SDK...
@@ -22,12 +22,12 @@ echo Installing Qt6 via choco...
 choco install qtcreator -y --no-progress
 choco install qt6 -y --no-progress --params="ADD_TO_PATH=1"
 setx QT_DIR "C:\Qt\6.9.0\msvc2022_64" /M
-setx PATH "%PATH%;C:\Qt\6.9.0\msvc2022_64\bin" /M
+setx PATH "%%PATH%%;C:\Qt\6.9.0\msvc2022_64\bin" /M
 
 refreshenv
 
 setx QT_DIR "C:\Qt\6.9.0\msvc2022_64" /M
-setx PATH "%PATH%;C:\Qt\6.9.0\msvc2022_64\bin" /M
+setx PATH "%%PATH%%;C:\Qt\6.9.0\msvc2022_64\bin" /M
 
 refreshenv
 
@@ -37,15 +37,15 @@ where qmake
 where sqlite3
 
 echo Adding environment variables to current session...
-set PATH=%PATH%;%ProgramFiles%\LibreOffice\program
-set PATH=%PATH%;%ProgramFiles%\SQLite
-set PATH=%PATH%;%QT_DIR%\bin
+set PATH=%%PATH%%;%%ProgramFiles%%\LibreOffice\program
+set PATH=%%PATH%%;%%ProgramFiles%%\SQLite
+set PATH=%%PATH%%;%%QT_DIR%%\bin
 
 refreshenv
 
 echo Required environment variables:
-echo - Qt6_DIR: %QT_DIR%
-echo - LibreOffice_DIR: %LibreOffice_DIR%
-echo - SQLite3_DIR: %SQLite3_DIR%
+echo - Qt6_DIR: %%QT_DIR%%
+echo - LibreOffice_DIR: %%LibreOffice_DIR%%
+echo - SQLite3_DIR: %%SQLite3_DIR%%
 
 refreshenv
