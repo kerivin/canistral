d0b8c91156948171f9d5c9da5ae16190a410757b
kerivin <root@LAPTOP-50JRBB77.>
2025-05-19 17:05:45 +0300

    build experiments

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -3,18 +3,26 @@ echo Installing LibreOffice SDK...
 choco install libreoffice-sdk -y --params="/AddToPath"
 setx LibreOffice_DIR "C:\Program Files\LibreOffice\sdk" /M
 
+refreshenv
+
 echo Installing Qt6 via choco...
 choco install qtcreator -y
 choco install qt6 -y --params="ADD_TO_PATH=1"
 setx QT_DIR "C:\Qt\6.9.0\msvc2022_64" /M
 setx PATH "%PATH%;C:\Qt\6.9.0\msvc2022_64\bin" /M
 
+refreshenv
+
 setx QT_DIR "C:\Qt\6.9.0\msvc2022_64" /M
 setx PATH "%PATH%;C:\Qt\6.9.0\msvc2022_64\bin" /M
+
+refreshenv
 
 echo Installing SQLite3...
 choco install sqlite --installargs="ADDLOCAL=IncludeFiles,LibraryFiles" -y
 setx SQLite3_DIR "C:\Program Files\SQLite" /M
+
+refreshenv
 
 echo Verifying installations...
 where libreoffice
@@ -26,8 +34,10 @@ set PATH=%PATH%;%ProgramFiles%\LibreOffice\program
 set PATH=%PATH%;%ProgramFiles%\SQLite
 set PATH=%PATH%;%QT_DIR%\bin
 
+refreshenv
+
 echo Required environment variables:
-echo - QT_DIR: %QT_DIR%
+echo - Qt6_DIR: %QT_DIR%
 echo - LibreOffice_DIR: %LibreOffice_DIR%
 echo - SQLite3_DIR: %SQLite3_DIR%
 
