61ee6d67dd2a3ea009ab11f1635d39352e622ad6
keringo <>
2025-05-24 19:32:01 +0300

    python venv -> install v3

--- main.cpp
+++ main.cpp
@@ -28,6 +28,10 @@ int main(int argc, char *argv[])
 
 		try
 		{
+			py::exec(R"(
+				import os
+				os.environ['EXECJS_RUNTIME'] = 'node'
+				)");
 			py::module_ sys = py::module_::import("sys");
 
 			fs::path exe_dir = fs::path(argv[0]).parent_path();
